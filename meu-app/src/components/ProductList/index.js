4d8de1b060bd1851ed7e6a9d46a026ff4f22113f
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-07-17 15:42:17 -0300

    Update index.js

--- meu-app/src/components/ProductList/index.js
+++ meu-app/src/components/ProductList/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from '../Product'
 
 const ProductList = () => {
-  const [categoriaTab, setCategoriaTab] = useState('Hamburguer');
+  const [categoriaTab, setCategoriaTab] = useState ('Hamburguer');
 
   return (
     <section className='my-12 max-w-screen-xl mx-auto px-3'>
       {/* Menu de categoria*/}
 
       <div className='flex items-center justify-center space-x-6'>
-        <p className={categoriaTab === 'Hamburguer' ? "active-menu-tab bg-primary" : 'menu-tab'} onclick={() => setCategoriaTab('Hamburguer')}>Hamburguer</p>
-        <p className={categoriaTab === 'Brasileira' ? "active-menu-tab bg-primary" : 'menu-tab'} onclick={() => setCategoriaTab('Brasileira')} >Brasileira</p>
-        <p className={categoriaTab === 'Japonesa' ? "active-menu-tab bg-primary" : 'menu-tab'} onclick={() => setCategoriaTab('Japonesa')} >Japonesa</p>
+        <p className={categoriaTab === 'Hamburguer' ? 'active-menu-tab bg-primary' : 'menu-tab'} onclick={() => setCategoriaTab('Hamburguer')}>Hamburguer</p>
+        <p className={categoriaTab === 'Brasileira' ? 'active-menu-tab bg-primary' : 'menu-tab'} onclick={() => setCategoriaTab('Brasileira')} >Brasileira</p>
+        <p className={categoriaTab === 'Japonesa' ? 'active-menu-tab bg-primary' : 'menu-tab'} onclick={() => setCategoriaTab('Japonesa')} >Japonesa</p>
 
       </div>
       {/* lista de produtos*/}
