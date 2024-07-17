c9ec694e0e3520262e71431a09556f7ef6791827
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-07-17 15:31:16 -0300

    Feat: ProductList

--- meu-app/src/components/ProductList/index.js
+++ meu-app/src/components/ProductList/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Product from '../Product'
 
 const ProductList = () => {
-  // const [categoriaTab, setCategoriaTab] = useState('Hambuguer');
+  const [categoriaTab, setCategoriaTab] = useState('Hamburguer');
 
   return (
     <section className='my-12 max-w-screen-xl mx-auto px-3'>
       {/* Menu de categoria*/}
 
       <div className='flex items-center justify-center space-x-6'>
-        <p className='active-menu-tab bg-primary'>Hamburguer</p>
-        <p className='menu-tab'>Brasileira</p>
-        <p className='menu-tab'>Japonesa</p>
+        <p className={categoriaTab === 'Hamburguer' ? "active-menu-tab bg-primary" : 'menu-tab'} onclick={() => setCategoriaTab('Hamburguer')}>Hamburguer</p>
+        <p className={categoriaTab === 'Brasileira' ? "active-menu-tab bg-primary" : 'menu-tab'} onclick={() => setCategoriaTab('Brasileira')} >Brasileira</p>
+        <p className={categoriaTab === 'Japonesa' ? "active-menu-tab bg-primary" : 'menu-tab'} onclick={() => setCategoriaTab('Japonesa')} >Japonesa</p>
 
       </div>
       {/* lista de produtos*/}
