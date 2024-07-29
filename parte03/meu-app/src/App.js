50e01f1722a5e0c1a4bf3fe975c8e1c33b3ef22c
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-07-29 15:07:34 -0300

    Feat: Projeto por pastas

---
+++ parte03/meu-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
