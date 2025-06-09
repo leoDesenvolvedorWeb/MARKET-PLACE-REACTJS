3081c00f6f0d2da5847a1708a28e8c206073ffef
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2025-06-09 19:26:30 -0300

    Feat: continuando e nova pasta

---
+++ parte15/meu-app/src/pages/Admin/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <section className='my-12 max-w-screen-xl mx-auto px-6'>
+      <div className=''>
+
+
+      </div>
+
+    </section>
+  )
+}
+
+export default Admin
